c061e9e4bdc15dfb9bb07baabf6c8f15e7d3c5b2
rsotang <rsotang@gmail.com>
2023-06-20 12:34:21 +0000

    + funcion de sortear nombres

--- To_PET.py
+++ To_PET.py
@@ -1,3 +1,7 @@
+import os
+import pydicom
+import numpy as np
+from scipy.ndimage import zoom
 import re  # Módulo de expresiones regulares
 
 def ordenar_alfanumericamente(lista):
@@ -5,7 +9,61 @@ def ordenar_alfanumericamente(lista):
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
     return sorted(lista, key=alphanum_key)
 
-mi_lista = ['ExportImg_65.DCM', 'ExportImg_66.DCM', 'ExportImg_67.DCM', 'ExportImg_68.DCM', 'ExportImg_69.DCM', 'ExportImg_70.DCM', 'ExportImg_71.DCM', 'ExportImg_72.DCM', 'ExportImg_73.DCM', 'ExportImg_74.DCM', 'ExportImg_75.DCM', 'ExportImg_76.DCM', 'ExportImg_77.DCM', 'ExportImg_78.DCM', 'ExportImg_79.DCM', 'ExportImg_80.DCM', 'ExportImg_81.DCM', 'ExportImg_1.DCM', 'ExportImg_2.DCM', 'ExportImg_3.DCM', 'ExportImg_4.DCM', 'ExportImg_5.DCM', 'ExportImg_6.DCM', 'ExportImg_7.DCM', 'ExportImg_8.DCM', 'ExportImg_9.DCM', 'ExportImg_10.DCM', 'ExportImg_11.DCM', 'ExportImg_12.DCM', 'ExportImg_13.DCM', 'ExportImg_14.DCM', 'ExportImg_15.DCM', 'ExportImg_16.DCM', 'ExportImg_17.DCM', 'ExportImg_18.DCM', 'ExportImg_19.DCM', 'ExportImg_20.DCM', 'ExportImg_21.DCM', 'ExportImg_22.DCM', 'ExportImg_23.DCM', 'ExportImg_24.DCM', 'ExportImg_25.DCM', 'ExportImg_26.DCM', 'ExportImg_27.DCM', 'ExportImg_28.DCM', 'ExportImg_29.DCM', 'ExportImg_30.DCM', 'ExportImg_31.DCM', 'ExportImg_32.DCM', 'ExportImg_33.DCM', 'ExportImg_34.DCM', 'ExportImg_35.DCM', 'ExportImg_36.DCM', 'ExportImg_37.DCM', 'ExportImg_38.DCM', 'ExportImg_39.DCM', 'ExportImg_40.DCM', 'ExportImg_41.DCM', 'ExportImg_42.DCM', 'ExportImg_43.DCM', 'ExportImg_44.DCM', 'ExportImg_45.DCM', 'ExportImg_46.DCM', 'ExportImg_47.DCM', 'ExportImg_48.DCM', 'ExportImg_49.DCM', 'ExportImg_50.DCM', 'ExportImg_51.DCM', 'ExportImg_52.DCM', 'ExportImg_53.DCM', 'ExportImg_54.DCM', 'ExportImg_55.DCM', 'ExportImg_56.DCM', 'ExportImg_57.DCM', 'ExportImg_58.DCM', 'ExportImg_59.DCM', 'ExportImg_60.DCM', 'ExportImg_61.DCM', 'ExportImg_62.DCM', 'ExportImg_63.DCM', 'ExportImg_64.DCM']
-mi_lista_ordenada = ordenar_alfanumericamente(mi_lista)
+# Define las rutas a las carpetas
+carpeta1 = "SPECT Symbia"
 
-print(mi_lista_ordenada)
+carpeta2 = "PET"
+carpeta_destino = "prueba"
+
+# Obtiene las listas de archivos en las carpetas
+archivos_carpeta1 = os.listdir(carpeta1)
+archivos_carpeta2 = os.listdir(carpeta2)
+
+archivos_carpeta1= ordenar_alfanumericamente(archivos_carpeta1)
+archivos_carpeta2= ordenar_alfanumericamente(archivos_carpeta2)
+
+print(archivos_carpeta1)
+
+print(archivos_carpeta2)
+
+# Itera sobre los archivos en las carpetas
+for item in range(len(archivos_carpeta1)):
+    # Carga las dos imágenes
+    ds1 = pydicom.dcmread(os.path.join(carpeta1, archivos_carpeta1[item]))
+    ds2 = pydicom.dcmread(os.path.join(carpeta2, archivos_carpeta2[item]))
+
+    # Accede a la data de los píxeles
+    pixel_data1 = ds1.pixel_array
+    pixel_data2 = ds2.pixel_array
+
+    # Calcula los factores de zoom para cada dimensión
+    zoom_factor_y = pixel_data2.shape[0] / pixel_data1.shape[0]
+    zoom_factor_x = pixel_data2.shape[1] / pixel_data1.shape[1]
+
+    # Usa la función zoom de SciPy para cambiar la resolución de la imagen
+    pixel_data1_resized = zoom(pixel_data1, (zoom_factor_y, zoom_factor_x))
+
+    # Actualiza la data de los píxeles en la segunda imagen con la data de los píxeles redimensionada de la primera imagen
+    ds2.PixelData = pixel_data1_resized.tobytes()
+
+    # Actualiza también las etiquetas que indican las dimensiones de la imagen
+    ds2.Rows, ds2.Columns = pixel_data1_resized.shape
+
+    #Parece que hay un problema con la ordenacion de los frames de los stacks, en el poster del HUBU lo mencionan
+    #Quizas haya que renombrar la etiqueta dle frame number según la iteración del bucle para asegurarnos, o algo por el estilo
+
+
+    #Creo que la ordenación de los frames tiene que ver con el orden de la lectura de los nombres de los archivos, i.e , se lee
+    # export_1, despues export_10 en vez de export_2. Tengo que comprobarlo. Si es así se puede reajustar el nombre de los       archivos , o ya si que sí cambiar el nombre de las etiquetas de frame y total frame
+
+
+    ds2[0x0054, 0x0081].value = len(archivos_carpeta1)
+    ds2[0x0054, 0x1330].value = item
+
+  #Aun habiendo cambiado eso parece que el problema parte más del orden de importación
+  #Para ello he sorteado los nombres con expresiones regulares
+    # Guarda la imagen modificada en la carpeta de destino
+    ds2.save_as(os.path.join(carpeta_destino, archivos_carpeta1[item]))
+    print(archivos_carpeta1[item])
+    print(archivos_carpeta2[item])
+print('\n sacabo')
